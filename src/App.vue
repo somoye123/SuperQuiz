3a66fa0c122060205cf92fac743e2bd2fb6cca21
Somoye Ayotunde <somoye.ayotunde@gmail.com>
2019-09-26 13:46:24 +0100

    Initial commit of the finished super quiz app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1 class="text-center">The Super Quiz</h1>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                mode: 'app-question'
+            }
+        },
+        methods: {
+          answered(isCorrect) {
+              if (isCorrect) {
+                  this.mode = 'app-answer';
+              } else {
+                  this.mode = 'app-question';
+                  alert('Wrong, try again!');
+              }
+          }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .flip-enter {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-enter-active {
+        animation: flip-in  0.5s ease-out forwards;
+    }
+
+    .flip-leave {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-leave-active {
+        animation: flip-out 0.5s ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
 </style>
